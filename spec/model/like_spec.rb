4dfde2196e6bf71b037687dc22525d9120f912f2
techmoves <muhndlovu28@gmail.com>
2023-08-26 21:37:32 +0200

    add test for update coment and likes methods

--- spec/model/like_spec.rb
+++ spec/model/like_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe Like, type: :model do
     post.reload
     expect(post.likes_counter).to eq(1)
   end
+
+describe '#update_likes_counter' do
+    it 'increments the post likes_counter by 1' do
+      user = User.create!(name: 'User Name', posts_counter: 0)
+      post = Post.create!(author: user, title: 'Post Title', comments_counter: 0, likes_counter: 0)
+      like = Like.new(author: user, post:)
+
+      expect(post.likes_counter).to eq 0
+
+      like.save!
+      post.reload
+
+      expect(post.likes_counter).to eq 1
+    end
+  end
+
 end
