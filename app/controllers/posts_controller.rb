04587b4e1f8a99274d05db38ff0d8639591af2df
techmoves <muhndlovu28@gmail.com>
2023-08-31 15:41:52 +0200

    Add post and users and updated the  controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+     @posts = User.find_by(id: params['user_id'])
+  end
 
-  def show; end
+  def show
+    @posts = User.find_by(id: params['user_id']).posts.find_by(id: params['id'])
+    @comment = Comment.where(post_id: @posts)
+  end
 end
