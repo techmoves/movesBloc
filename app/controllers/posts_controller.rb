b6ab32a142c53019b37bbaf08cde979b7410f420
techmoves <muhndlovu28@gmail.com>
2023-09-01 23:59:22 +0200

    display comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,14 @@ class PostsController < ApplicationController
     @posts = User.find_by(id: params['user_id'])
   end
 
-  def new; end
+  def new
+    @user = self
+  end
 
   def show
     @user = User.find_by(id: params['user_id']).posts.find_by(id: params['id'])
     @comment = Comment.where(post_id: @posts)
+    @comment = Comment.where(author_id: @user.id)
   end
+
 end
