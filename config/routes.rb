9b0a95934a184c9177af6ecfe94ccaa04e0d0940
techmoves <muhndlovu28@gmail.com>
2023-09-01 21:21:24 +0200

    Fix the rout issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
  root "users#index"
 
- resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :viewpost, :show,]
+ resources :users, only: [:index, :show, :posts,] do
+    resources :posts, only: [:index, :new, :posts, :show,]
   end
   # Defines the root path route ("/")
   # root "articles#index"
