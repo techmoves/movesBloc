b30066a36718a3822cf577fb82aee228109580c7
techmoves <muhndlovu28@gmail.com>
2023-09-01 21:03:06 +0200

    Add changes requierd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def new; end
 
   def show
-    @user = User.find_by(id: params['user_id']).posts.find_by(id: params['id'])
+    @posts = User.find_by(id: params['user_id']).posts.find_by(id: params['id'])
     @comment = Comment.where(post_id: @posts)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ root "users#index"
+
  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :show,]
+    resources :posts, only: [:index, :new, :viewpost, :show,]
   end
   # Defines the root path route ("/")
   # root "articles#index"
